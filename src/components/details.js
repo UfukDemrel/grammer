202f41b81f295165323d2544b8ce22ae3f9a2ba3
UfukDemrel <udemirel012@gmail.com>
2024-04-24 22:50:15 +0300

    first commit

--- src/components/details.js
+++ src/components/details.js
@@ -4,9 +4,15 @@ import { Link, useParams } from "react-router-dom";
 const DetailsPage = () => {
   const { lessonId } = useParams();
   const [lesson, setLesson] = useState(null);
-  const [showSecondText, setShowSecondText] = useState(false);
   const [userAnswerText1, setUserAnswerText1] = useState("");
   const [userAnswerText2, setUserAnswerText2] = useState("");
+  const [userAnswerText3, setUserAnswerText3] = useState("");
+  const [userAnswerText4, setUserAnswerText4] = useState("");
+  const [userAnswerText5, setUserAnswerText5] = useState("");
+  const [showSecondText, setShowSecondText] = useState(false);
+  const [showThirdText, setShowThirdText] = useState(false);
+  const [showFourText, setshowFourText] = useState(false);
+  const [showFiveText, setshowFiveText] = useState(false);
   const [isRed, setIsRed] = useState(false);
 
   useEffect(() => {
@@ -21,12 +27,6 @@ const DetailsPage = () => {
           .find((lesson) => lesson.id === lessonId);
         setLesson(selectedLesson);
         setShowSecondText(false);
-        setUserAnswerText1(
-          localStorage.getItem(`${lessonId}-userAnswerText1`) || ""
-        );
-        setUserAnswerText2(
-          localStorage.getItem(`${lessonId}-userAnswerText2`) || ""
-        );
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -46,21 +46,36 @@ const DetailsPage = () => {
         } else if (textType === "text2") {
           setUserAnswerText2(userVoiceInput);
           localStorage.setItem(`${lessonId}-userAnswerText2`, userVoiceInput);
+          setShowThirdText(true);
+          startRecognition("text3");
+        } else if (textType === "text3") {
+          setUserAnswerText3(userVoiceInput);
+          setshowFourText(true);
+          localStorage.setItem(`${lessonId}-userAnswerText3`, userVoiceInput);
+        }
+        else if (textType === "text4") {
+          setUserAnswerText4(userVoiceInput);
+          setshowFiveText(true);
+          localStorage.setItem(`${lessonId}-userAnswerText4`, userVoiceInput);
+        }
+        else if (textType === "text5") {
+          setUserAnswerText5(userVoiceInput);
+          localStorage.setItem(`${lessonId}-userAnswerText5`, userVoiceInput);
         }
         setShowSecondText(true);
         startRecognition(textType);
       };
-  
-      const blinkInterval = 500;
+
+      const blinkInterval = 300;
       const blinkIntervalId = setInterval(() => {
         setIsRed((prevState) => !prevState);
       }, blinkInterval);
-  
+
       recognition.onend = () => {
         clearInterval(blinkIntervalId);
         setIsRed(false);
       };
-  
+
       recognition.start();
     } catch (error) {
       console.error("Error starting recognition:", error);
@@ -90,7 +105,7 @@ const DetailsPage = () => {
     return <div>Loading...</div>;
   }
 
-  const { text1, text2 } = lesson.states[0];
+  const { text1, text2, text3, text4, text5 } = lesson.states[0];
 
   return (
     <div>
@@ -118,15 +133,47 @@ const DetailsPage = () => {
         {!showSecondText && (
           <>
             <div className="flex justify-center mt-2 border-2 fixed bottom-0 rounded-t-xl text-center w-full p-4 m-0 right-0 shadow items-center">
-              <button className="flex" onClick={() => handleButtonClick("text1")}>
-              <div onClick={() => setIsRed(!isRed)}>
-                {isRed ? <div className="red"></div> : ''}
-              </div>
-              <svg width="2.5rem" height="2.5rem" viewBox="0 0 1024 1024" className="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M698.624 501.376a170.112 170.112 0 0 1-170.112 170.112h-58.496a170.112 170.112 0 0 1-170.112-170.112V250.048a170.048 170.048 0 0 1 170.112-170.112h58.496a170.112 170.112 0 0 1 170.112 170.112v251.328z" fill="#979FCB" /><path d="M725.76 410.624v104.512c0 104.448-87.68 189.12-195.904 189.12H462.464c-108.224 0-195.968-84.672-195.968-189.12V410.624h-31.808v123.904c0 118.912 99.904 215.296 223.04 215.296h76.736c123.264 0 223.104-96.384 223.104-215.296V410.624h-31.808z" fill="#394B97" /><path d="M453.184 719.104h92.16v162.944h-92.16z" fill="#394B97" /><path d="M734.656 944v-12.224c0-40.896-38.976-74.048-87.232-74.048h-296.32c-48.128 0-87.232 33.216-87.232 74.048v12.224h470.784z" fill="#394B97" /><path d="M520.896 225.088h174.464v41.152H520.896zM298.944 225.088h174.464v41.152H298.944zM520.896 297.536h174.464v41.152H520.896zM298.944 297.536h174.464v41.152H298.944zM520.896 375.68h174.464v41.216H520.896zM298.944 375.68h174.464v41.216H298.944zM520.896 448.256h174.464v41.088H520.896zM298.944 448.256h174.464v41.088H298.944z" fill="#5161A4" /></svg>
+              <button
+                className="flex"
+                onClick={() => handleButtonClick("text1")}
+              >
+                <div onClick={() => setIsRed(!isRed)}>
+                  {isRed ? <div className="red"></div> : ""}
+                </div>
+                <svg
+                  width="2.5rem"
+                  height="2.5rem"
+                  viewBox="0 0 1024 1024"
+                  className="icon"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M698.624 501.376a170.112 170.112 0 0 1-170.112 170.112h-58.496a170.112 170.112 0 0 1-170.112-170.112V250.048a170.048 170.048 0 0 1 170.112-170.112h58.496a170.112 170.112 0 0 1 170.112 170.112v251.328z"
+                    fill="#979FCB"
+                  />
+                  <path
+                    d="M725.76 410.624v104.512c0 104.448-87.68 189.12-195.904 189.12H462.464c-108.224 0-195.968-84.672-195.968-189.12V410.624h-31.808v123.904c0 118.912 99.904 215.296 223.04 215.296h76.736c123.264 0 223.104-96.384 223.104-215.296V410.624h-31.808z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M453.184 719.104h92.16v162.944h-92.16z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M734.656 944v-12.224c0-40.896-38.976-74.048-87.232-74.048h-296.32c-48.128 0-87.232 33.216-87.232 74.048v12.224h470.784z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M520.896 225.088h174.464v41.152H520.896zM298.944 225.088h174.464v41.152H298.944zM520.896 297.536h174.464v41.152H520.896zM298.944 297.536h174.464v41.152H298.944zM520.896 375.68h174.464v41.216H520.896zM298.944 375.68h174.464v41.216H298.944zM520.896 448.256h174.464v41.088H520.896zM298.944 448.256h174.464v41.088H298.944z"
+                    fill="#5161A4"
+                  />
+                </svg>
               </button>
             </div>
           </>
         )}
+
         {showSecondText && (
           <div>
             {userAnswerText1 && (
@@ -139,12 +186,43 @@ const DetailsPage = () => {
             <div className="bg-green-200 p-2 font-medium text-left text-xs rounded-lg shadow-md mb-2 w-1/2 text-black">
               {text2}
             </div>
-            <div className="flex justify-center mt-2 border-2 fixed bottom-0 rounded-t-xl text-center w-full p-4 m-0 right-0 shadow items-center">
+            <div className="z-10 flex justify-center mt-2 border-2 fixed bottom-0 rounded-t-xl text-center w-full p-4 m-0 right-0 shadow items-center">
               <button
                 className="flex justify-center mt-2"
                 onClick={() => handleButtonClick("text2")}
               >
-                <svg width="2.5rem" height="2.5rem" viewBox="0 0 1024 1024" className="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M698.624 501.376a170.112 170.112 0 0 1-170.112 170.112h-58.496a170.112 170.112 0 0 1-170.112-170.112V250.048a170.048 170.048 0 0 1 170.112-170.112h58.496a170.112 170.112 0 0 1 170.112 170.112v251.328z" fill="#979FCB" /><path d="M725.76 410.624v104.512c0 104.448-87.68 189.12-195.904 189.12H462.464c-108.224 0-195.968-84.672-195.968-189.12V410.624h-31.808v123.904c0 118.912 99.904 215.296 223.04 215.296h76.736c123.264 0 223.104-96.384 223.104-215.296V410.624h-31.808z" fill="#394B97" /><path d="M453.184 719.104h92.16v162.944h-92.16z" fill="#394B97" /><path d="M734.656 944v-12.224c0-40.896-38.976-74.048-87.232-74.048h-296.32c-48.128 0-87.232 33.216-87.232 74.048v12.224h470.784z" fill="#394B97" /><path d="M520.896 225.088h174.464v41.152H520.896zM298.944 225.088h174.464v41.152H298.944zM520.896 297.536h174.464v41.152H520.896zM298.944 297.536h174.464v41.152H298.944zM520.896 375.68h174.464v41.216H520.896zM298.944 375.68h174.464v41.216H298.944zM520.896 448.256h174.464v41.088H520.896zM298.944 448.256h174.464v41.088H298.944z" fill="#5161A4" /></svg>
+                <div onClick={() => setIsRed(!isRed)}>
+                  {isRed ? <div className="red"></div> : ""}
+                </div>
+                <svg
+                  width="2.5rem"
+                  height="2.5rem"
+                  viewBox="0 0 1024 1024"
+                  className="icon"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M698.624 501.376a170.112 170.112 0 0 1-170.112 170.112h-58.496a170.112 170.112 0 0 1-170.112-170.112V250.048a170.048 170.048 0 0 1 170.112-170.112h58.496a170.112 170.112 0 0 1 170.112 170.112v251.328z"
+                    fill="#979FCB"
+                  />
+                  <path
+                    d="M725.76 410.624v104.512c0 104.448-87.68 189.12-195.904 189.12H462.464c-108.224 0-195.968-84.672-195.968-189.12V410.624h-31.808v123.904c0 118.912 99.904 215.296 223.04 215.296h76.736c123.264 0 223.104-96.384 223.104-215.296V410.624h-31.808z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M453.184 719.104h92.16v162.944h-92.16z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M734.656 944v-12.224c0-40.896-38.976-74.048-87.232-74.048h-296.32c-48.128 0-87.232 33.216-87.232 74.048v12.224h470.784z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M520.896 225.088h174.464v41.152H520.896zM298.944 225.088h174.464v41.152H298.944zM520.896 297.536h174.464v41.152H520.896zM298.944 297.536h174.464v41.152H298.944zM520.896 375.68h174.464v41.216H520.896zM298.944 375.68h174.464v41.216H298.944zM520.896 448.256h174.464v41.088H520.896zM298.944 448.256h174.464v41.088H298.944z"
+                    fill="#5161A4"
+                  />
+                </svg>
               </button>
             </div>
             {userAnswerText2 && (
@@ -156,6 +234,169 @@ const DetailsPage = () => {
             )}
           </div>
         )}
+
+        {showThirdText && (
+          <div>
+          <div className="bg-green-200 p-2 font-medium text-left text-xs rounded-lg shadow-md mb-2 w-1/2 text-black">
+              {text3}
+            </div>
+            {userAnswerText3 && (
+              <div className="flex justify-end items-center">
+                <div className="bg-orange-200 w-max p-2 font-medium text-left text-xs rounded-lg shadow-md mb-2 text-black">
+                  {userAnswerText3}
+                </div>
+              </div>
+            )}
+            <div className="z-10 flex justify-center mt-2 border-2 fixed bottom-0 rounded-t-xl text-center w-full p-4 m-0 right-0 shadow items-center">
+              <button
+                className="flex justify-center mt-2"
+                onClick={() => handleButtonClick("text3")}
+              >
+                <div onClick={() => setIsRed(!isRed)}>
+                  {isRed ? <div className="red"></div> : ""}
+                </div>
+                <svg
+                  width="2.5rem"
+                  height="2.5rem"
+                  viewBox="0 0 1024 1024"
+                  className="icon"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M698.624 501.376a170.112 170.112 0 0 1-170.112 170.112h-58.496a170.112 170.112 0 0 1-170.112-170.112V250.048a170.048 170.048 0 0 1 170.112-170.112h58.496a170.112 170.112 0 0 1 170.112 170.112v251.328z"
+                    fill="#979FCB"
+                  />
+                  <path
+                    d="M725.76 410.624v104.512c0 104.448-87.68 189.12-195.904 189.12H462.464c-108.224 0-195.968-84.672-195.968-189.12V410.624h-31.808v123.904c0 118.912 99.904 215.296 223.04 215.296h76.736c123.264 0 223.104-96.384 223.104-215.296V410.624h-31.808z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M453.184 719.104h92.16v162.944h-92.16z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M734.656 944v-12.224c0-40.896-38.976-74.048-87.232-74.048h-296.32c-48.128 0-87.232 33.216-87.232 74.048v12.224h470.784z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M520.896 225.088h174.464v41.152H520.896zM298.944 225.088h174.464v41.152H298.944zM520.896 297.536h174.464v41.152H520.896zM298.944 297.536h174.464v41.152H298.944zM520.896 375.68h174.464v41.216H520.896zM298.944 375.68h174.464v41.216H298.944zM520.896 448.256h174.464v41.088H520.896zM298.944 448.256h174.464v41.088H298.944z"
+                    fill="#5161A4"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        )}
+
+        {showFourText && (
+          <div>
+          <div className="bg-green-200 p-2 font-medium text-left text-xs rounded-lg shadow-md mb-2 w-1/2 text-black">
+              {text4}
+            </div>
+            {userAnswerText4 && (
+              <div className="flex justify-end items-center">
+                <div className="bg-orange-200 w-max p-2 font-medium text-left text-xs rounded-lg shadow-md mb-2 text-black">
+                  {userAnswerText4}
+                </div>
+              </div>
+            )}
+            <div className="z-10 flex justify-center mt-2 border-2 fixed bottom-0 rounded-t-xl text-center w-full p-4 m-0 right-0 shadow items-center">
+              <button
+                className="flex justify-center mt-2"
+                onClick={() => handleButtonClick("text4")}
+              >
+                <div onClick={() => setIsRed(!isRed)}>
+                  {isRed ? <div className="red"></div> : ""}
+                </div>
+                <svg
+                  width="2.5rem"
+                  height="2.5rem"
+                  viewBox="0 0 1024 1024"
+                  className="icon"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M698.624 501.376a170.112 170.112 0 0 1-170.112 170.112h-58.496a170.112 170.112 0 0 1-170.112-170.112V250.048a170.048 170.048 0 0 1 170.112-170.112h58.496a170.112 170.112 0 0 1 170.112 170.112v251.328z"
+                    fill="#979FCB"
+                  />
+                  <path
+                    d="M725.76 410.624v104.512c0 104.448-87.68 189.12-195.904 189.12H462.464c-108.224 0-195.968-84.672-195.968-189.12V410.624h-31.808v123.904c0 118.912 99.904 215.296 223.04 215.296h76.736c123.264 0 223.104-96.384 223.104-215.296V410.624h-31.808z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M453.184 719.104h92.16v162.944h-92.16z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M734.656 944v-12.224c0-40.896-38.976-74.048-87.232-74.048h-296.32c-48.128 0-87.232 33.216-87.232 74.048v12.224h470.784z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M520.896 225.088h174.464v41.152H520.896zM298.944 225.088h174.464v41.152H298.944zM520.896 297.536h174.464v41.152H520.896zM298.944 297.536h174.464v41.152H298.944zM520.896 375.68h174.464v41.216H520.896zM298.944 375.68h174.464v41.216H298.944zM520.896 448.256h174.464v41.088H520.896zM298.944 448.256h174.464v41.088H298.944z"
+                    fill="#5161A4"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        )}
+
+        {showFiveText && (
+          <div>
+          <div className="bg-green-200 p-2 font-medium text-left text-xs rounded-lg shadow-md mb-2 w-1/2 text-black">
+              {text5}
+            </div>
+            {userAnswerText5 && (
+              <div className="flex justify-end items-center">
+                <div className="bg-orange-200 w-max p-2 font-medium text-left text-xs rounded-lg shadow-md mb-2 text-black">
+                  {userAnswerText5}
+                </div>
+              </div>
+            )}
+            <div className="flex z-10 bg-white justify-center mt-2 border-2 fixed bottom-0 rounded-t-xl text-center w-full p-4 m-0 right-0 shadow items-center">
+              <button
+                className="flex justify-center mt-2"
+                onClick={() => handleButtonClick("text5")}
+              >
+                <div onClick={() => setIsRed(!isRed)}>
+                  {isRed ? <div className="red"></div> : ""}
+                </div>
+                <svg
+                  width="2.5rem"
+                  height="2.5rem"
+                  viewBox="0 0 1024 1024"
+                  className="icon"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M698.624 501.376a170.112 170.112 0 0 1-170.112 170.112h-58.496a170.112 170.112 0 0 1-170.112-170.112V250.048a170.048 170.048 0 0 1 170.112-170.112h58.496a170.112 170.112 0 0 1 170.112 170.112v251.328z"
+                    fill="#979FCB"
+                  />
+                  <path
+                    d="M725.76 410.624v104.512c0 104.448-87.68 189.12-195.904 189.12H462.464c-108.224 0-195.968-84.672-195.968-189.12V410.624h-31.808v123.904c0 118.912 99.904 215.296 223.04 215.296h76.736c123.264 0 223.104-96.384 223.104-215.296V410.624h-31.808z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M453.184 719.104h92.16v162.944h-92.16z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M734.656 944v-12.224c0-40.896-38.976-74.048-87.232-74.048h-296.32c-48.128 0-87.232 33.216-87.232 74.048v12.224h470.784z"
+                    fill="#394B97"
+                  />
+                  <path
+                    d="M520.896 225.088h174.464v41.152H520.896zM298.944 225.088h174.464v41.152H298.944zM520.896 297.536h174.464v41.152H520.896zM298.944 297.536h174.464v41.152H298.944zM520.896 375.68h174.464v41.216H520.896zM298.944 375.68h174.464v41.216H298.944zM520.896 448.256h174.464v41.088H520.896zM298.944 448.256h174.464v41.088H298.944z"
+                    fill="#5161A4"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        )}
+
       </div>
     </div>
   );
